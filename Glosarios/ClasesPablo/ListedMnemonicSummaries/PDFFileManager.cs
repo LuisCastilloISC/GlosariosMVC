969128efc366e1427e19a21fe0466cdf2f73140a
Pablo Lavín <thepabloface@hotmail.com>
2019-05-07 19:07:52 -0500

    Se agrega el PDFFileManger. Tiene el método Generate con los parámetros Nombre del Glosario e Idioma.

---
+++ Glosarios/ClasesPablo/ListedMnemonicSummaries/PDFFileManager.cs
@@ -0,0 +1,47 @@
+﻿using iTextSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListedMnemonicSummaries
+{
+    public abstract class PDFFileManager
+    {
+        public static void Generate(string strTopic,string strLanguage)
+        {
+            string strTXTDirectory = "";
+            string strPDFDirectory = "";
+            switch (strLanguage)
+            {
+                case "Español":
+                    if (System.IO.File.Exists(@"Resúmenes Mnemotécnicos Listados\" + strTopic + ".txt"))
+                    {
+                        strTXTDirectory = @"Resúmenes Mnemotécnicos Listados\" + strTopic + ".txt";
+                        strPDFDirectory = @"Resúmenes Mnemotécnicos Listados\" + strTopic + ".pdf";
+                    }
+                    else
+                        throw new Exception(strTopic + ".txt no existe.");
+                    break;
+                default:
+                case "English":
+                    if (System.IO.File.Exists(@"Listed Mnemonic Summaries\" + strTopic + ".txt"))
+                    {
+                        strTXTDirectory = @"Listed Mnemonic Summaries\" + strTopic + ".txt";
+                        strPDFDirectory = @"Listed Mnemonic Summaries\" + strTopic + ".pdf";
+                    }
+                    else
+                        throw new Exception(strTopic + ".txt doesn't exist.");
+                    break;
+            }
+
+            using (iTextSharp.text.Document aDocument = new iTextSharp.text.Document())
+            {
+                iTextSharp.text.pdf.PdfWriter.GetInstance(aDocument, new System.IO.FileStream(strPDFDirectory, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.ReadWrite));
+                aDocument.Open();
+                aDocument.Add(new iTextSharp.text.Paragraph(ListedTextFileManager.Read(strTXTDirectory)));
+            }
+        }
+    }
+}
